5d204afacbf5c680d186a9bf31489de297c8eff1
Rehan Raihan <68391710+raihanuddin2002@users.noreply.github.com>
2021-11-27 12:20:37 +0600

    Cloud Connection

--- ajax/config.php
+++ ajax/config.php
@@ -1,3 +1,4 @@
 <?php
-   $connection = mysqli_connect('localhost', 'root', '','ss') or die('Connection Failed');
+   // $connection = mysqli_connect('localhost', 'root', '','ss') or die('Connection Failed');
+   $connection = mysqli_connect('sql6.freemysqlhosting.net', 'sql6454409', 'fQFCAMwC6e','sql6454409') or die('Connection Failed');
 ?>
